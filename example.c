5181373c00238c063eb429be7b0d3f4fad64ef1b
QSXW <47172719+QSXW@users.noreply.github.com>
2021-04-10 16:55:59 +0800

    Add files via upload

--- example.c
+++ example.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 #include <slimage.h>
 #include <stdlib.h>
+#include <time.h>
+
+#define DEBUG
 
 int main(int argc, char **argv)
 {
+    clock_t start, end;
     Frame frame;
     if (!argv[1])
     {   
-        argv[1] = "D:\\Code\\C\\slmedia/data/images/jpeg/lol1.jpg";
+        argv[1] = "D:\\Code\\C\\slmedia/data/images/jpeg/IMG_0135.JPG";
     }
-    if (!(frame = ImageRead(argv[1]))) 
+    start = clock();
+    if (!(frame = ImageRead(argv[1], NULL, 0))) 
     {
         fprintf(stderr, "usage: %s [The absolute path for image]\n", argv[0]);
         return 0;
     }
-
+    end = clock();
     ImageShow(frame);
     slFrameDeallocator(frame);
     
